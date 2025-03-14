433d73517ede91113df94f558b7040979890951a
Francesco <you@example.com>
2025-03-14 12:35:50 +0100

    Add milestone 2

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@
         Una lista <ul> statica che viene creata a partire da un array di stringhe (animals) dove ciascuna stringa rappresenta il nome di un animale.
 
 Obiettivo: Mostrare la struttura base della lista di animali con un <details> che può essere espanso o contratto. */
+
+//!  Milestone 2: Aggiungere Animali Casuali
+
+/* Trasforma l’array animals usando useState (l’array è inizialmente vuoto).
+    Aggiungi un bottone "Aggiungi Animale" sopra il <details>.
+    Cliccando il bottone, un animale casuale viene aggiunto alla lista.
+    Usa un array predefinito per scegliere casualmente: */
