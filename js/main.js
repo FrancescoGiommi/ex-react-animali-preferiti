109fa27b603834acf4e083e9c1f7d37802f99d0d
Francesco <you@example.com>
2025-03-16 14:43:41 +0100

    Add milestone 3

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ Obiettivo: Mostrare la struttura base della lista di animali con un <details> ch
     Aggiungi un bottone "Aggiungi Animale" sopra il <details>.
     Cliccando il bottone, un animale casuale viene aggiunto alla lista.
     Usa un array predefinito per scegliere casualmente: */
+
+//! Milestone 3: Usare una Modale per Aggiungere Animali
+
+/*  Partendo da questo componente Modal:
+    Espandilo affinché:
+
+    La vecchia prop content può essere usata per passare un componente qualsiasi.
+    Un nuovo div in fondo alla modale contiene il bottone Annulla e un nuovo bottone Conferma.
+    Una nuova prop onConfirm si aspetta una funzione per gestire l’azione di conferma.
+
+    Sostituisci l’aggiunta casuale dell’animale con una modale interattiva:
+        Cliccando il bottone "Aggiungi Animale," si apre una modale.
+        La modale include un input di testo (passato al prop content) per inserire il nome di un animale.
+        Conferma: Aggiunge l’animale alla lista e chiude la modale.
+        Annulla: Chiude la modale senza modificare la lista.
+
+Obiettivo: L’utente può aggiungere animali specifici utilizzando la modale. */
